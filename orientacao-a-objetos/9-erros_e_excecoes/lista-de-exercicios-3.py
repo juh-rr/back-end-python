bb7c314e69dd7f7d05e1584d96b3b3297a002284
Juliana Ramalho <juhramalho.r@gmail.com>
2024-02-04 22:05:05 -0300

    Resolução do exercício 3

--- orientacao-a-objetos/9-erros_e_excecoes/lista-de-exercicios-3.py
+++ orientacao-a-objetos/9-erros_e_excecoes/lista-de-exercicios-3.py
@@ -2,19 +2,23 @@
 ## No entanto, ele não está funcionando bem. Você pode consertá-lo?
 
 def calcular_media(valores):
-    tamanho = 1
+    tamanho = 0
     soma = 0.0
-    for i, valor in enumerate(valores):
-        soma += valor
-        i += 1
-    media = soma / tamanho
+    media_calculada = 0
+    for i in range(len(valores)):
+        soma += valores[i]
+        tamanho += 1
+    media_calculada = soma / tamanho
+    return media_calculada
 
 continuar = True
 valores = []
 while continuar:
     valor = input('Digite um número para entrar na sua média ou "ok" para calcular o valor:')
     if valor.lower() == 'ok':
-        continuar = false
+        continuar = False
+    elif valor.isdigit():
+        valores.append(int(valor))
 
 media = calcular_media(valores)
 print('A média calculada para os valores {} foi de {}'.format(valores, media))
